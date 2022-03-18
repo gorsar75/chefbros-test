7408bb1f774db30e3a02f893af3e01b1c25b1cb9
gorsar75 <92424358+gorsar75@users.noreply.github.com>
2022-03-18 14:15:34 -0700

    Installed react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <section className="w-3/6 m-auto flex flex-wrap mt-20">
+     <div className="flex flex-wrap">
+       <div className="w-full mb-8 p-10 bg-gray-100 relative rounded shadow">
+         <span className="text-gray-300 text-6xl absolute italic right-10">
+           01
+         </span>
+         <h2 className="z-10 relative text-2xl text-gray-900 absolute">
+           ChefBros
+         </h2>
+         <hr className="block mb-6 mt-5 w-8 border-red-500 h-0 border-t-2" />
+         <p className="text-gray-700">
+             [ ] Register as a customer [ ] Register as a chef [ ] User Portal [ ] View services [ ] Select the cook
+             [ ] Send requests to cook [ ] Track the cooks [ ] Get details about the chef’s location, expertise, experience & per hour charges
+             [ ] Specify the time duration [ ] Make payments [ ] Ratings and reviews [ ] Chef Portal [ ] Manage their profile
+             [ ] Show specialities [ ] Show price [ ] Set booking calendar [ ] View user request [ ] Accept/decline user request [ ] Receive payments
+         </p>
+       </div>
+     </div>
+   </section>
+
   );
 }
 
